47f573bfb2d79a638cd0c19ee79017035cd3cf4f
bigmonstershift <biz76216@gmail.com>
2020-07-17 15:32:25 +0900

    add search function

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,4 +2,11 @@ class ToppagesController < ApplicationController
   def index
 		@questions = Question.order(id: :desc).page(params[:page]).per(10)
   end
+
+	def search
+		@searchword = params[:search][:searchwords]
+		@questions = Question.where("content LIKE ?", "%#{@searchword}%").order(id: :desc).page(params[:page]).per(10)
+		@count_questions = @questions.count
+		render 'search'
+	end
 end
