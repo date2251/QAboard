dfd67feaf09c92964415af8a0c0fd35a178eae3d
bigmonstershift <biz76216@gmail.com>
2020-07-18 17:30:09 +0900

    add tag function

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,12 +1,25 @@
 class ToppagesController < ApplicationController
+	before_action :tags, only: [:index, :search]
+
   def index
 		@questions = Question.order(id: :desc).page(params[:page]).per(10)
   end
 
 	def search
 		@searchword = params[:search][:searchwords]
-		@questions = Question.where("content LIKE ?", "%#{@searchword}%").order(id: :desc).page(params[:page]).per(10)
+		@categoryword = params[:search][:categorywords]
+		if @searchword
+			@questions = Question.where("content LIKE ?", "%#{@searchword}%").order(id: :desc).page(params[:page]).per(10)
+		else
+			@questions = Question.where("tag LIKE ?", "%#{@categoryword}%").order(id: :desc).page(params[:page]).per(10)
+		end
 		@count_questions = @questions.count
 		render 'search'
+	end
+
+	private
+
+	def tags
+		@tags = Question.select(:tag).distinct
 	end
 end
